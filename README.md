386ebe96d5ebcd575dd62b620073fcc20ce69052
McCabe Northup <mccabenorthup@gmail.com>
2019-12-13 22:29:39 -0700

    Added new java file. Old java file had imposibbly long if statments. Replaced with an 'arrow' function

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # javaScript_Homework_3
+
+##Over_View
+
+I found this password generator extremely difficult. I Made several different variations to this one. Currently using several sources. 
+
+My biggest hurdle is definitely using the input from the user of length of password. From there I cannot pull the value as an integer. I have used parseInt(), + before the element. 
---
+++ script1.js
@@ -0,0 +1,80 @@
+// Assignment Code
+var generateBtn = document.getElementById('generate');
+var resultEl = document.getElementById("password");
+var upperEl = document.getElementById("upper");
+var lowerEl = document.getElementById("lower");
+var symbolEl = document.getElementById("symbol");
+var numberEl = document.getElementById("num");
+var clippyEl = document.getElementById("clipboard");
+var lengthEl = document.getElementById("long")
+
+var randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
+
+clippyEl.addEventListener('click', () => {
+	var textarea = document.createElement('textarea');
+	var password = resultEl.innerText;
+	
+	if(!password) { return; }
+	
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
+});
+
+generate.addEventListener('click', () => {
+	var length = +lengthEl.value;
+	var hasLower = lowerEl.checked;
+	var hasUpper = upperEl.checked;
+	var hasNumber = numberEl.checked;
+	var hasSymbol = symbolEl.checked;
+	
+	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = '';
+	var typesCount = lower + upper + number + symbol;
+	var typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	// Doesn't have a selected type
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			var funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	var finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
+
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	var symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
